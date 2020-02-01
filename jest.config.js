1a8d44fa687a2e284b06bb6d21cced0085455491
Zsolt Sándor <zsolt.sandor@gmail.com>
2020-02-01 20:15:18 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rootDir: process.cwd(),
+    transform: { '^.+\\.ts?$': 'ts-jest' },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverage: true
+};
